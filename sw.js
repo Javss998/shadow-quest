8eb5672e2a58f25b4fccbf7566376af41c7eb71e
Javss998 <javierpmpj@gmail.com>
2026-05-29 21:58:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shadowquest-v1';
+const CACHE = 'shadowquest-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,20 +8,31 @@ const ASSETS = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request)
+      .then(cached => {
+        // Network first for HTML (always fresh), cache for assets
+        if (e.request.destination === 'document') {
+          return fetch(e.request)
+            .then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; })
+            .catch(() => cached);
+        }
+        return cached || fetch(e.request).catch(() => caches.match('./index.html'));
+      })
   );
 });
