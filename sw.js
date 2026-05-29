d059a1a8e1abe9acc545ba72adba913e5bf7958c
Javss998 <javierpmpj@gmail.com>
2026-05-29 23:27:29 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'shadowquest-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => {
+        // Network first for HTML (always fresh), cache for assets
+        if (e.request.destination === 'document') {
+          return fetch(e.request)
+            .then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; })
+            .catch(() => cached);
+        }
+        return cached || fetch(e.request).catch(() => caches.match('./index.html'));
+      })
+  );
+});
